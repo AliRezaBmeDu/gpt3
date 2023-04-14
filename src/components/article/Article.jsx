06701810fc1644306d36b437ef7e2d7d0ccd8179
AliReza <ali.reza.buet@gmail.com>
2023-04-14 12:32:19 +0600

    Necessary Components created

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './article.css'
+
+const article = () => {
+  return (
+    <div>article</div>
+  )
+}
+
+export default article
---
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatGPT3.css'
+const WhatGPT3 = () => {
+  return (
+    <div>WhatGPT3</div>
+  )
+}
+
+export default WhatGPT3
